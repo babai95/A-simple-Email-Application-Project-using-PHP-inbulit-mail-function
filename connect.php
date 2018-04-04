d4723ce39bf650894f9c1c21136ff9de4b6e4ee3
Sayantan Gupta <guptasayantan95@gmail.com>
2018-04-04 08:43:47 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+$dbname="email_app";
+$dbuser="email_admin";
+$dbpass="yCeGPdy7SxoUdPHc";
+$dbhost="localhost";
+$dbc=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname)
+or die(mysqli_error($dbc));
+?>
